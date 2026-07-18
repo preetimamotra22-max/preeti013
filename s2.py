143f48182e66476283304dfa8f0ea4f2209c5103
preetimamotra22-max <preetimamotra22@gmail.com>
2026-07-18 10:51:35 +0530

    Update s2.py

--- s2.py
+++ s2.py
@@ -274,13 +274,13 @@ if menu == "Visualization":
                         df_view['Active'].sum()
                     ]
                 }
-          total_df = pd.DataFrame(total_data)
+           total_df = pd.DataFrame(total_data)
                 
-          fig1 = px.pie(total_df, values='Count', names='Status', 
+           fig1 = px.pie(total_df, values='Count', names='Status', 
                               hole=0.4, color_discrete_sequence=['#FF4B4B','#8B0000','#00CC96','#636EFA'])
-          fig1.update_traces(textinfo='percent+label')
-          fig1.update_layout(paper_bgcolor="#0e1117", plot_bgcolor="#0e1117", font_color="white")
-          st.plotly_chart(fig1, use_container_width=True)
+           fig1.update_traces(textinfo='percent+label')
+           fig1.update_layout(paper_bgcolor="#0e1117", plot_bgcolor="#0e1117", font_color="white")
+           st.plotly_chart(fig1, use_container_width=True)
             
             # 2. TOP 10 COUNTRIES PIE
         with col2:
